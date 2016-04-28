2ab113dfeed0ad804f5a7325a407f2e6d4918eab
prashanthmotifworks <prashanth@motifworks.com>
2016-04-28 12:55:25 +0530

    added images

--- wapp/server/startup.js
+++ wapp/server/startup.js
@@ -3,3 +3,9 @@ Meteor.startup(function() {
        privatekey: '6LcTeRsTAAAAAM1nFrufK_6zt-NcpYhWElZh0qSQ'
     });
 });
+
+
+//domain:  6LcTeRsTAAAAAM1nFrufK_6zt-NcpYhWElZh0qSQ
+
+
+ //localhost: '6LcTeRsTAAAAAPV6p3TfUkBJfjHRAknhsqR-vUUt'
